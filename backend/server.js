e9eb13a711046d1f4e002f5514f16399b1a61862
anishDalvi <anishdalvi2001@gmail.com>
2022-11-20 20:39:54 +0530

    Prepare Deploy

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require ('express')
 const dotenv = require('dotenv').config()
+const path = require('path')
 const colors = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
@@ -15,6 +16,15 @@ app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
 
+// Server frontend 
+
+if ( process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'../frontend/dist/index.html')))
+} else{
+    app.get('/', (req, res) => res.send('Not in Production'))
+}
 
 app.use(errorHandler)
 
